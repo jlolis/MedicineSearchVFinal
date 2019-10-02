af68cf0ae5e2360c6a1f65ee397efd82800d8763
guilhermevieira05 <guilherme.vieira05@outlook.com>
2019-10-02 20:21:54 -0300

    Mudança de layout total e login efetuado

--- app/src/main/java/com/example/joaoelolis/medicinesearchv2/MainActivity.java
+++ app/src/main/java/com/example/joaoelolis/medicinesearchv2/MainActivity.java
@@ -1,19 +1,27 @@
 package com.example.joaoelolis.medicinesearchv2;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 
+import com.firebase.ui.auth.AuthUI;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     private FirebaseDatabase firebaseDatabase;
     private DatabaseReference databaseReference;
+    private SharedPreferences sharedPreferences;
     private EditText editTextNome;
     private EditText editTextBula;
     private EditText editTextObservacoes;
@@ -28,10 +36,63 @@ public class MainActivity extends AppCompatActivity {
         editTextNome = findViewById(R.id.edtCadastraNome);
         editTextBula = findViewById(R.id.edtBula);
         editTextObservacoes = findViewById(R.id.edtObservacao);
+        sharedPreferences = getSharedPreferences("LOGIN", Context.MODE_PRIVATE);
+        String resultado = sharedPreferences.getString("LOGIN","");
+
+        if(!Boolean.parseBoolean(resultado)){
+            criarLogin();
+        }
 
         conectarBanco();
 
     }
+
+
+    private void criarLogin(){
+        List<AuthUI.IdpConfig> providers = Arrays.asList(
+                new AuthUI.IdpConfig.EmailBuilder().build(),
+                new AuthUI.IdpConfig.PhoneBuilder().build(),
+                new AuthUI.IdpConfig.GoogleBuilder().build()
+        );
+
+
+        startActivityForResult(
+                AuthUI.getInstance()
+                        .createSignInIntentBuilder()
+                        .setAvailableProviders(providers)
+                        .setIsSmartLockEnabled(false)
+                        .setLogo(R.drawable.ic_login)
+                        .setTheme(R.style.LoginThemes)
+                        .build(), 123
+        );
+
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode == 123 && resultCode == RESULT_OK){
+
+            sharedPreferences = getSharedPreferences("LOGIN", Context.MODE_PRIVATE);
+            SharedPreferences.Editor editor = sharedPreferences.edit();
+            editor.putString("LOGIN","true");
+            editor.apply();
+
+        } else{
+            finish();
+        }
+    }
+
+    public void logout (View v){
+        sharedPreferences = getSharedPreferences("LOGIN", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putString("LOGIN","false");
+        editor.apply();
+        criarLogin();
+    }
+
 
     private void conectarBanco(){
         FirebaseApp.initializeApp(MainActivity.this);
